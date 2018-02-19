cddf741157f1114ccaa3b944cc12e8a51d72818d
cuatro4 <cchang30@seattlecentral.edu>
2018-02-19 14:59:43 -0800

    Add a nicely formatted table

--- StampCard.py
+++ StampCard.py
@@ -1,3 +1,7 @@
+from tabulate import tabulate
+
+# TODO: Put the dependencies into a venv so that it's easy to install
+
 class StampCard:
     # A stamp card for an establishment which contains a name and a capacity
     # Authors: Cody Antonio Gagnon & Chuan-Li Ojales Chang
@@ -17,19 +21,12 @@ class StampCard:
 
     def print_stamps(self):
         stamps = []
-        for x in range(3, self.stamp_capacity):
-            stamps.append(["X"] * self.stamp_capacity)
-        for row in stamps:
-            # concatenates each string
-            print (" ".join([str(row)]))
-
-    def stamp_row(self):
-        return (1, len(self.stamp_capacity) - 1)
-
-    def stamp_col(self):
-        return (1, len(self.stamp_capacity) - 1)
+        stamps_per_row = self.stamp_capacity / 2  # hardcoded rows for now...
+        for x in range(1, 3):
+            stamps.append(["X"] * stamps_per_row)
+        print (tabulate(stamps, tablefmt="grid"))  # takes a list of lists or another tabular data type as the first argument, and outputs a nicely formatted table
 
 
-oasis = StampCard("Oasis", 5)
-print("Name: " + oasis.name() + ", Capacity in each row: " + oasis.capacity())
+oasis = StampCard("Oasis", 10)
+print("Name: " + oasis.name() + ", Capacity: " + oasis.capacity())
 oasis.print_stamps()
