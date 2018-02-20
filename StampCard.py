c3af388c4d9827554add8cc4b72e53e921072929
Cody Gagnon <cgagno01@seattlecentral.edu>
2018-02-19 16:20:30 -0800

    add getters, bad emoji support

--- StampCard.py
+++ StampCard.py
@@ -1,35 +1,71 @@
+# -*- coding: utf-8 -*-
 from tabulate import tabulate
-
 # TODO: Put the dependencies into a venv so that it's easy to install
+# TODO: add the amount of stamps we currently have vs. the stamp_capacity
+
 
 class StampCard:
     # A stamp card for an establishment which contains a name and a capacity
     # Authors: Cody Antonio Gagnon & Chuan-Li Ojales Chang
 
-    def __init__(self, card_name, stamp_capacity):
-        self.card_name = card_name
+    def __init__(self, card_name, card_description, stamp_capacity,
+                 stamp_symbol, redeemable_location):
+        """
+        Initialize the stamp card with a:
+            name
+            description
+            capacity
+            symbol for stamping
+            location to be redeemed
+        """
+        self.name = card_name
+        self.description = card_description
         self.stamp_capacity = stamp_capacity
+        self.stamp_symbol = stamp_symbol
+        self.location = redeemable_location
 
-    def name(self):
+    def get_name(self):
         """
         Return the name of the stampcard
         """
-        return self.card_name
+        return self.name
 
-    def capacity(self):
+    def get_description(self):
+        """
+        Return the description of the stampcard
+        """
+        return self.description
+
+    def get_capacity(self):
         """
         Return the capacity as a string.
         """
         return str(self.stamp_capacity)
 
+    def get_symbol(self):
+        """
+        Return the capacity as a string.
+        """
+        return self.stamp_symbol
+
+    def get_location(self):
+        """
+        Return the redeemable location as a string.
+        """
+        return self.location
+
     def print_stamps(self):
         stamps = []
         stamps_per_row = self.stamp_capacity / 2  # hardcoded rows for now...
         for x in range(1, 3):
-            stamps.append(["X"] * stamps_per_row)
-        print (tabulate(stamps, tablefmt="grid"))  # takes a list of lists or another tabular data type as the first argument, and outputs a nicely formatted table
+            stamps.append([self.stamp_symbol] * stamps_per_row)
+        print (tabulate(stamps, tablefmt="grid").encode('utf8', 'replace'))  # takes a list of lists or another tabular data type as the first argument, and outputs a nicely formatted table
 
 
-oasis = StampCard("Oasis", 10)
-print("Name: " + oasis.name() + ", Capacity: " + oasis.capacity())
-oasis.print_stamps()
+def main():
+    stamp = unicode("🍭", 'utf8')
+    oasis = StampCard("Oasis", "Bubble Tea", 10, stamp, "University District")
+    print("Name: " + oasis.get_name() + ", Capacity: " + oasis.get_capacity())
+    oasis.print_stamps()
+
+main()
